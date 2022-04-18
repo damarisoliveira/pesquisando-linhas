2bdfb313d54719b730f747e0b2ec6283d2e573d1
Damaris <damarisoliveira@users.noreply.github.com>
2022-04-18 00:11:03 -0300

    projeto

---
+++ isk/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace isk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.MaskedTextBox();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(91, 40);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(100, 20);
+            this.txtBox.TabIndex = 2;
+            // 
+            // Grid
+            // 
+            this.Grid.AllowUserToAddRows = false;
+            this.Grid.AllowUserToDeleteRows = false;
+            this.Grid.CausesValidation = false;
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Location = new System.Drawing.Point(53, 88);
+            this.Grid.Name = "Grid";
+            this.Grid.RowHeadersVisible = false;
+            this.Grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.Grid.Size = new System.Drawing.Size(381, 114);
+            this.Grid.TabIndex = 3;
+            this.Grid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Grid_CellDoubleClick);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(isk.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 316);
+            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtBox;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+    }
+}
+
---
+++ isk/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace isk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DataRow _obj = ((DataRowView)Grid.CurrentRow.DataBoundItem).Row;
+
+            pesquisarPorCodigo();
+
+         
+        }
+        void pesquisarPorCodigo()
+        {
+            bool achou = false;
+            Grid.ClearSelection();
+            foreach (DataGridViewRow row in Grid.Rows) {
+
+                DataRow obj = ((DataRowView)row.DataBoundItem).Row;
+
+                if (txtBox.Text == obj["Codigo"].ToString())
+                {
+                    achou = true;
+                    selecionarCelulasDaLinha(row);  
+                    
+                }
+            }
+            if (!achou)
+            {
+                MessageBox.Show("Não achou!!!");
+            }
+        }
+
+        void selecionarCelulasDaLinha(DataGridViewRow row)
+        {
+            foreach (DataGridViewTextBoxCell cell in row.Cells)
+            {
+                cell.Selected = true;
+            }
+        }
+
+        class EtiquetaConferenciaProduto
+        {
+          
+            public int Codigo { get; set; }
+            public string Produto { get; set; }
+
+            public EtiquetaConferenciaProduto(int codigo, string produto)
+            {
+                Codigo = codigo;
+                Produto = produto;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DataTable tb = new DataTable();
+            tb.Columns.Add(new DataColumn("Codigo", typeof(Int32)));
+            tb.Columns.Add(new DataColumn("Produto", typeof(string)));
+           
+            DataRow rw1 = tb.NewRow();
+            rw1["Codigo"] = 24442;
+            rw1["Produto"] = "Lampada";
+
+            DataRow rw2 = tb.NewRow();
+            rw2["Codigo"] = 23221;
+            rw2["Produto"] = "Bicicleta";
+
+            DataRow rw3 = tb.NewRow();
+            rw3["Codigo"] = 23221;
+            rw3["Produto"] = "Bicicleta";
+
+            tb.Rows.Add(rw1);
+            tb.Rows.Add(rw2);
+            tb.Rows.Add(rw3);
+
+            
+            //Grid.AutoGenerateColumns = false;
+            Grid.DataSource = tb;
+
+            Grid.ClearSelection();
+        }
+
+        private void Grid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //DataRow _row = ((DataRowView)Grid.Rows[e.RowIndex].DataBoundItem).Row;
+           // MessageBox.Show("ensagem saida = " + _row);
+
+
+        }
+    }
+}
